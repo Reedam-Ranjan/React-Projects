0b206a9506fc70d8f282ac1d85278cbb5df8e0c0
Reedam Ranjan <reedamranjan@gmail.com>
2023-08-15 17:57:11 +0530

    changed-height

--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -6,7 +6,7 @@ const style = {
   borderRadius: "10px",
   width: "250px",
   height: "250px",
-  marginTop: "50px",
+  marginTop: "100px",
   margin: "0 auto",
   display: "grid",
   gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
